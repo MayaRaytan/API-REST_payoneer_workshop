e7feff16d3ce81ee9f67247a26f19743d1cde9d8
MayaRaytan <mayaraytan@mail.tau.ac.il>
2022-12-01 19:21:07 +0200

    "API-REST workshop"

--- app.py
+++ app.py
@@ -24,18 +24,20 @@ def add_item():
     items.append(item)
     return Response(str(item), content_type="application/json")
 
+
 @app.route('/items/<id>', methods=["DELETE"])
 def delete_item(id):
-    if (id in items):
+    if id in items:
         items.remove(id)
     else:
         return "", 400
     return "", 200
 
+
 @app.route('/items/<id>', methods=['PATCH'])
 def update_item(id):
     item = next(filter(lambda x: x.id == id, items), None)
-    if (item is not None):
+    if item is not None:
         body = request.get_json()
         name = body["name"]
         item.name = name
