eed80f743b5bb18c205cf63edf7ce24accdfd3d2
MayaRaytan <mayaraytan@mail.tau.ac.il>
2022-12-01 18:53:42 +0200

    "API-REST workshop"

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from .data.seed import items
+from .models.item import Item
 
 app = Flask("app",
             static_url_path='', 
@@ -14,3 +15,29 @@ def root():
 @app.route("/items")
 def get_items():
     return Response(str(items), content_type="application/json")
+
+@app.route('/items', methods=["POST"])
+def add_item():
+    body = request.get_json()
+    name = body["name"]
+    item = Item(name)
+    items.append(item)
+    return Response(str(item), content_type="application/json")
+
+@app.route('/items/<id>', methods=["DELETE"])
+def delete_item(id):
+    if (id in items):
+        items.remove(id)
+    else:
+        return "", 400
+    return "", 200
+
+@app.route('/items/<id>', methods=['PATCH'])
+def update_item(id):
+    item = next(filter(lambda x: x.id == id, items), None)
+    if (item is not None):
+        body = request.get_json()
+        name = body["name"]
+        item.name = name
+        return "", 200
+    return "", 400
